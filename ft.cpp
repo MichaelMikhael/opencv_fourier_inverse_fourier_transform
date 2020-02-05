7c91b2a0b6050967353d3b2d5c31a16dcb70bb4e
Michael Mikhael <38666025+MichaelMikhael@users.noreply.github.com>
2020-02-05 12:05:38 -0700

    Update ft.cpp

--- ft.cpp
+++ ft.cpp
@@ -1,7 +1,45 @@
-#include "ft.hpp"
+#include <opencv4/opencv2/imgproc/imgproc.hpp>
+#include <opencv4/opencv2/highgui/highgui.hpp>
+#include <opencv4/opencv2/video/video.hpp>
+#include <opencv4/opencv2/video/tracking.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <stdio.h>
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/core/core.hpp>
+#include <opencv4/opencv2/imgproc/imgproc_c.h>
+#include <opencv4/opencv2/highgui/highgui_c.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include "fourier_trans.hpp"
 
+using namespace std;
+using namespace cv;
 
 int main()
 {
-Ftrans("/home/mmwm/Desktop/IVIL.jpg",500,500,1);	//Ftrans(image,image_length,image_width,filter_true_orfalse);
+	Mat im,f;
+	VideoCapture v(0);
+	DFtrans df;
+	
+	while(1)
+	{
+		v>>im;
+		resize(im,im,Size(500,500));
+		im = Scalar::all(0);
+		ellipse(im,Point(250,250),Size(25,35),45,0,360,Scalar(255,255,255),2,8);
+		
+		f = df.Trans(im);
+		
+		imshow("im",im);
+		imshow("f",f);
+		waitKey(30);
+	}
+	waitKey(0);
 }
